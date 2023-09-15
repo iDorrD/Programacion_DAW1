e6014d753c7f779c3c80ac743bf2aae597a301d4
iDorrD <dorrd2002@gmail.com>
2023-09-15 12:06:43 +0200

    Add test java class

---
+++ src/Test1/test.java
@@ -0,0 +1,15 @@
+package Test1;
+
+public class test {
+    public static void main(String[] args) {
+        int a=3;
+        int b=3;
+        if (a>b){
+            System.out.println(a+" es mayor que "+b);
+        } else if (a<b) {
+            System.out.println(a+" es menor que "+b);
+        }else {
+            System.out.println("ES IGUAL");
+        }
+    }
+}
