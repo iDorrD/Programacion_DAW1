39f7a333640eb8c12216bab9c9aa6baf5a0aa179
iDorrD <dorrd2002@gmail.com>
2023-09-24 17:59:00 +0200

    se modifico el EjercicioExtra2.java

--- src/Teoria/Unidad1/EjercicioExtra2.java
+++ src/Teoria/Unidad1/EjercicioExtra2.java
@@ -2,6 +2,7 @@ package Teoria.Unidad1;
 
 public class EjercicioExtra2 {
     public static void main(String[] args) {
+        // Creamos las constantes de cada mes, indicando su número:
         final int ENERO = 1;
         final int FEBRERO = 2;
         final int MARZO = 3;
@@ -15,25 +16,26 @@ public class EjercicioExtra2 {
         final int NOVIEMBRE = 11;
         final int DICIEMBRE = 12;
 
+        // Creamos constantes de cada dato importante:
         final int SEGUNDOS_POR_MINUTO = 60;
         final int TAMANO_DE_COMPUTADORA_MB = 2048;
         final boolean VALOR_LOGICO_VERDADERO = true;
         final double RAZON_FAHRENHEIT_A_CELSIUS = 1.8;
         final double PRECIO_EN_EUROS = 3.8;
 
-        System.out.println("MESES DEL AÑO ------------------------");
-        System.out.println("ENERO: " + ENERO);
-        System.out.println("FEBRERO: " + FEBRERO);
-        System.out.println("MARZO: " + MARZO);
-        System.out.println("ABRIL: " + ABRIL);
-        System.out.println("MAYO: " + MAYO);
-        System.out.println("JUNIO: " + JUNIO);
-        System.out.println("JULIO: " + JULIO);
-        System.out.println("AGOSTO: " + AGOSTO);
-        System.out.println("SEPTIEMBRE: " + SEPTIEMBRE);
-        System.out.println("OCTUBRE: " + OCTUBRE);
-        System.out.println("NOVIEMBRE: " + NOVIEMBRE);
-        System.out.println("DICIEMBRE: " + DICIEMBRE);
+        System.out.printf("%17s","MESES DEL AÑO\n");
+        System.out.println("Enero: " + ENERO);
+        System.out.println("Febrero: " + FEBRERO);
+        System.out.println("Marzo: " + MARZO);
+        System.out.println("Abril: " + ABRIL);
+        System.out.println("Mayo: " + MAYO);
+        System.out.println("Junio: " + JUNIO);
+        System.out.println("Julio: " + JULIO);
+        System.out.println("Agosto: " + AGOSTO);
+        System.out.println("Septiembre: " + SEPTIEMBRE);
+        System.out.println("Octubre: " + OCTUBRE);
+        System.out.println("Noviembre: " + NOVIEMBRE);
+        System.out.println("Diciembre: " + DICIEMBRE);
 
         System.out.println("\nSegundos por minuto: " + SEGUNDOS_POR_MINUTO);
         System.out.println("Tamaño de la computadora en MB: " + TAMANO_DE_COMPUTADORA_MB);
