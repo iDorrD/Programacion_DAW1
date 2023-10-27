2e05c6bba016b9d9784e6b876fcc478f9d4ab74d
iDorrD <dorrd2002@gmail.com>
2023-10-27 14:42:04 +0200

    Arreglo menor del Ejercicio 2

--- src/UD2/Practica/EstructurasDeControlVariadas2/Ejercicio2.java
+++ src/UD2/Practica/EstructurasDeControlVariadas2/Ejercicio2.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        final int DOBLE=2;
+        final int TRIPLE=3;
         int numero;
         do {
             System.out.print("Introduce un número (Escribe 0 para salir): ");
             numero= teclado.nextInt();
-            System.out.println(numero);
-            if (numero>8){
-                System.out.println("¡Es mayor que 8! El doble es "+(numero*2));
+            System.out.println();
+            if (numero==0){
+                System.out.println("Fin");
+            }else if (numero>8){
+                System.out.println("¡Es mayor que 8! El doble de "+numero+" es "+(numero*DOBLE));
             }else {
-                System.out.println("¡Es menor que 8! El doble es "+(numero*3));
+                System.out.println("¡Es menor que 8! El triple de "+numero+" es "+(numero*TRIPLE));
             }
         }while (numero!=0);
-        System.out.println("Fin");
+
     }
 }
